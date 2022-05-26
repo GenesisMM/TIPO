d448c638872e3a18d581811796152ced8642ff36
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-26 09:26:22 -0700

    Customized base layer and removed unimplemented code

--- TIPO/src/App.js
+++ TIPO/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <header className="App-header">
-                    <h1>TIPO</h1>
-                      <button onclick="">
-                          Settings
-                      </button>
+            <h1>TIPO</h1>
+            <button onclick="">
+                Settings
+            </button>
         </header>
     </div>
   );
