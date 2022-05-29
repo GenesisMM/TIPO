81d1a766547c052c48003a0f6ad3e16554e79f94
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-28 19:44:01 -0700

    Change App from function to class. Added styling

--- TIPO/src/App.js
+++ TIPO/src/App.js
@@ -1,16 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <h1>TIPO</h1>
-            <button onclick="">
-                Settings
-            </button>
-        </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>TIPO</h1>
+                    <button onclick="">
+                        Settings
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
