9ba964f7a1494987ea2ac1b2d310b1a0a953d8da
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-29 10:45:11 -0700

    Added Setter and Getter Methods as well as comments

--- TIPO/src/App.js
+++ TIPO/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <h1>TIPO</h1>
-                    <p>{ this.projectOne.name }</p>
+                    <p>{ this.projectOne.getId() }</p>
                     <button onclick="">
                         Settings
                     </button>
--- TIPO/src/Project.js
+++ TIPO/src/Project.js
@@ -3,11 +3,32 @@ import './App.css';
 
 class Project {
 
+    // Default Constructor for Project Object
     constructor() {
         this.id = 0;
         this.name = "project";
         this.picture = "image.png";
         this.tasks = Array();
+    }
+
+    // Getter Method for project id
+    getId() {
+        return this.id;
+    }
+
+    // Setter Method for project id
+    setId(newId) {
+        this.id = newId;
+    }
+
+    // Getter Method for project name
+    getName() {
+        return this.name;
+    }
+
+    // Setter Method for project name
+    setName(newName) {
+        this.name = newName;
     }
 
 }
